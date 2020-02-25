c303ff00cf6050b7e69d818a05aa8e33d7754671
Amitay Isaacs <amitay@ozlabs.org>
2020-02-25 16:33:36 +1100

    plat: Log target if attribute access fails
    Signed-off-by: Amitay Isaacs <amitay@ozlabs.org>

--- hwpf/fapi2/src/plat/plat_utils.C
+++ hwpf/fapi2/src/plat/plat_utils.C
@@ -125,12 +125,15 @@ ReturnCode plat_access_attr_SETMACRO(const char *attr, struct pdbg_target *tgt, 
 
 	ret = attr_write(tgt, attr, val, size);
 	if (ret != 0) {
+		printf("Target = %s\n", path);
 		if (ret == ENOENT)
 			printf("Attribute '%s' not found for target '%s'\n", attr, path);
 		else if (ret == EMSGSIZE)
 			printf("Wrong size %zu for attribute '%s'\n", size, attr);
 		else if (ret == EIO)
 			printf("Failed to store attribute '%s'\n", attr);
+		else
+			printf("Unknown error %d writing attribute '%s'\n", ret, attr);
 
 		return FAPI2_RC_FALSE;
 	}
@@ -156,12 +159,15 @@ ReturnCode plat_access_attr_GETMACRO(const char *attr, struct pdbg_target *tgt, 
 
 	ret = attr_read(tgt, attr, val, size);
 	if (ret != 0) {
+		printf("Target = %s\n", path);
 		if (ret == ENOENT)
 			printf("Attribute '%s' not found for target '%s'\n", attr, path);
 		else if (ret == EMSGSIZE)
 			printf("Wrong size %zu for attribute '%s'\n", size, attr);
 		else if (ret == EINVAL)
 			printf("Value not set for attribute '%s'\n", attr);
+		else
+			printf("Unknown error %d reading attribute '%s'\n", ret, attr);
 
 		return FAPI2_RC_FALSE;
 	}
